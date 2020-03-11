7650892c7db210ed72e2958701e70ec932675eee
Skemc <eric.mico6@gmail.com>
2020-03-11 16:23:35 +0200

    ch(sass): - add sass loader - for using both sass ans css [Finishes: #170764519]

---
+++ src/store/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import Reducers from './reducers';
+
+const middleware = [thunk];
+
+const store = createStore(
+  Reducers,
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
+
+export default store;
