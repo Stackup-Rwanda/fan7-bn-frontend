70001498b830468724cfe88b4e72a56b1f35e176
Mutesi Sharon <sharonuase@gmail.com>
2020-03-11 07:36:30 +0200

    UI(setup redux): setup redux Setup and configured store seupt and configured root reducer [Finishes #170764515]

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,13 @@
-// create a store here
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import Reducers from './reducers'
+
+const middlewares = [thunk];
+
+const store = createStore(
+  Reducers,
+  composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+export default store;
